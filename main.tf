efba5c8f0a33cc0d0f8da2e94a6558e54d6f4b83
nihitha <nihithajumjuri@gmail.com>
2025-07-02 16:43:34 -0400

    bracnh

---
+++ main.tf
@@ -0,0 +1,22 @@
+# bootstrap/main.tf backend configuration
+
+provider "aws" {
+  region = "var.aws_region"
+}
+
+variable "aws_region" {
+  default = "ca-central-1"
+}
+
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "my-terraform-state-bucket"
+  acl    = "private"
+  region = "ca-central-1"
+  versioning {
+        enabled = true
+    }
+
+  tags = {
+    Name = "Terraform state bucket"
+  }
+}
