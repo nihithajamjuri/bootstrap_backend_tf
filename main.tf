b6fa9ed2c9fd4392c9fc7e271757d6ed5cf98a0e
nihitha <nihithajumjuri@gmail.com>
2025-07-02 17:39:07 -0400

    bucet name

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ variable "aws_region" {
 }
 
 resource "aws_s3_bucket" "tf_state" {
-  bucket = "my-terraform-state-bucket"
+  bucket = "nihi9-terraform-state-bucket"
   acl    = "private"
   region = "ca-central-1"
   versioning {
